abd6daee1a561c49746eda9a777d78706e1e4c3c
MegBritt6 <59936276+MegBritt6@users.noreply.github.com>
2020-01-30 13:46:39 -0500

    Update MyCount.sh

--- MyCount.sh
+++ MyCount.sh
@@ -1,7 +1,8 @@
 
 #!/bin/bash
 
-
+#Write a bash script called MyCount.sh that will loop through a sequence of 20 numbers. 
+#In each iteration the script will print out the number of the loop iteration AND whether it is a single- or double-digit number. 
 for Number in {1..20}
 do
 if [[ $Number == [[:digit:]] ]]
